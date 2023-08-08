53bedcfc0e336361a5a6d25e19786fcc097293b5
anabravn <bravinheloisa@gmail.com>
2023-08-08 17:48:13 -0300

    Argumentos de linha de comando

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"ui"
 	"ai"
+	"ui"
+
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/notnil/chess"
 	"github.com/veandco/go-sdl2/img"
@@ -21,6 +24,32 @@ func CreateWindow() *sdl.Window {
 }
 
 func main() {
+	var d int
+	var color chess.Color
+
+	args := os.Args[1:]
+
+	if len(args) < 2 {
+		fmt.Println("Usagem: ./chess-ai [profundidade] [cor]")
+		os.Exit(1)
+	} else {
+		depth, err := strconv.Atoi(args[0])
+		d = depth * 2
+
+		if err != nil {
+			fmt.Printf("\"%s\" não é uma profundidade válida\n", args[0])
+			os.Exit(1)
+		}
+
+		switch(args[1]) {
+		case "b", "black":
+			color = chess.Black
+		default:
+			color = ches.White
+		}
+	}
+
+
 	sdl.Init(sdl.INIT_EVERYTHING)
 	defer sdl.Quit()
 	img.Init(img.INIT_PNG)
@@ -29,12 +58,13 @@ func main() {
 	defer window.Destroy()
 	surface, _ := window.GetSurface()
 
-	gameUi := ui.NewGameUI()
+	gui := ui.NewGameUI()
 	game := chess.NewGame()
-	gameUi.Update(game)
+	gui.Update(game)
 
 	var nextMove *chess.Move = nil
 
+	over := false
 	running := true
 	for running {
 		event := sdl.WaitEventTimeout(10)
@@ -46,24 +76,24 @@ func main() {
 			}
 		}
 
-		if game.Position().Turn() == chess.White {
-		    nextMove = gameUi.GetSelectedMove(game)
+		if game.Position().Turn() == color {
+			nextMove = gui.GetSelectedMove(game)
 		} else {
-		    nextMove = ai.Search(game, 2)
+			nextMove = ai.Search(game, d)
 		}
 
 		if nextMove != nil {
-		    game.Move(nextMove)
-		    gameUi.Update(game)
-		    nextMove = nil
+			game.Move(nextMove)
+			gui.Update(game)
+			nextMove = nil
 		}
 
-		if game.Outcome() != chess.NoOutcome {
-		    fmt.Println(game.Outcome(), game.Method())
-		    running = false
+		if !over && game.Outcome() != chess.NoOutcome {
+			fmt.Println(game.Outcome(), game.Method())
+			over = true
 		}
 
-		gameUi.Draw(surface)
+		gui.Draw(surface)
 		window.UpdateSurface()
 	}
 }
